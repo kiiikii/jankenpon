26b6e81624db3653962edaa32a52a0180dcc5169
kiiikii <shiejacko@gmail.com>
2024-08-15 12:56:44 +0700

    add html js

---
+++ script.js
@@ -0,0 +1,8 @@
+/**
+ *  plan or pseucode for janken
+ *  1. make a rand.Math() {0:rock, 1:scissor, 2:paper} for computer
+ *  2. make a prompt for user input
+ *  3. the computer will choose first then user 
+ *  4. make a logic if user input beat computer rand.Math() then user win, otherwise if computer rand.Math() beat user input then computer win, otherwise will draw
+ *  5. the game will be best of 3 scoring from 0
+ */
